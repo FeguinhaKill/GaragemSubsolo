6536e62b1c22f4420bb30799d6dd475526f70166
FeguinhaKill <feguinhak2@gmail.com>
2026-05-22 16:40:55 -0300

    ?

--- database/factories/OrdemServicoItemFactory.php
+++ database/factories/OrdemServicoItemFactory.php
@@ -2,10 +2,7 @@
 
 namespace Database\Factories;
 
-
 use App\Models\OrdemServicoItens;
-use App\Models\Usuario;
-use App\Models\Funcionario;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -20,16 +17,8 @@ class OrdemServicoItensFactory extends Factory
      */
     public function definition(): array
     {
-            $status = rand(0, 1) ? 'Aberto' : 'Fechado';
-            $dataabertura = now()
-
-            return [
-            'usuario_id' => Usuario::all()->random()->id,
-            'funcionario_id' => Funcionario::all()->random()->id,
-            'data_abertura' => $this->now(),
-            'data_fechamento' => $this->faker->numberBetween(1, 5),
-            'status' => $this->$status,
-            'valor_total' => $this->faker->numberBetween(1, 5000),
+        return [
+            //
         ];
     }
 }
